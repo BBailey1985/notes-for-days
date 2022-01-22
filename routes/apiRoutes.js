6e2d0f6e81ee8db4c1b48d636f20a01cd9a8f5d2
BBailey1985 <brandon_bailey@hotmail.com>
2022-01-22 14:22:06 -0600

    stopping for the day

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,15 +5,28 @@ const { notes } = require('../db/notes.json')
 
 
 
+
 //reading notes.json file 
 router.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, notes));
 })
 
+router.get('/api/notes/:id', (req, res) => {
+  let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
+  res.json(storedNote[Number(req.params.id)]);
+})
+
 // post api
-router.post('/notes', (req, res) => {
-  console.log(req.body);
-  // res.sendFile(path.join(__dirname, notes));
+router.post('/api/notes', (req, res) => {
+  let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
+  let newNote = req.body;
+  let noteId = (storedNote.length).toString();
+  newNote = noteId;
+  storedNote.push(newNote);
+
+  fs.writeFileSync(notes, JSON.stringify(storedNote));
+  console.log(newNote)
+  res.json(storedNote);
 })
 
 module.exports = router;
