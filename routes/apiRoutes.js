9f6f8f8ac3db100ef169bf64c3ab0c9c4c2e899a
BBailey1985 <brandon_bailey@hotmail.com>
2022-01-24 10:51:56 -0600

    getting some help

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,42 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-const { notes } = require('../db/notes.json')
-
-
-
+const savedNotes = JSON.parse(fs.readFileSync('./db/notes.json', 'utf8'));
 
 //reading notes.json file 
 router.get('/api/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, notes));
-})
+  console.log('Hello!')
+  res.json(savedNotes)
 
-router.get('/api/notes/:id', (req, res) => {
-  let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
-  res.json(storedNote[Number(req.params.id)]);
-})
+//   // res.getFile(path.join(__dirname, notes));
+// })
 
-// post api
-router.post('/api/notes', (req, res) => {
-  let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
-  let newNote = req.body;
-  let noteId = (storedNote.length).toString();
-  newNote = noteId;
-  storedNote.push(newNote);
+router.get("/api/notes", function (req, res) {
+  fs.readFile("./db/notes.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(notes);
+  });
+});
 
-  fs.writeFileSync(notes, JSON.stringify(storedNote));
-  console.log(newNote)
-  res.json(storedNote);
-})
+// router.get('/api/notes/:id', (req, res) => {
+//   let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
+//   res.json(storedNote[Number(req.params.id)]);
+// })
+
+// // post api
+// router.post('/api/notes', (req, res) => {
+//   let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
+//   let newNote = req.body;
+//   let noteId = (storedNote.length).toString();
+//   newNote = noteId;
+//   storedNote.push(newNote);
+
+//   fs.writeFileSync(notes, JSON.stringify(storedNote));
+//   console.log(newNote)
+//   res.json(storedNote);
+// })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+// const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 //api routes
-app.use('/api', require('./routes/apiRoutes'));
-app.use('/', require('./routes/htmlRoutes'));
+app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+
 
 
 //listen port
