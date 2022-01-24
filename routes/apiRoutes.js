22cadb6f90366ade53ffa4846b1746381b420796
BBailey1985 <brandon_bailey@hotmail.com>
2022-01-24 12:57:05 -0600

    added delete function

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const uuid = require('../helpers/uuid')
 const fs = require('fs');
-const path = require('path');
 const savedNotes = JSON.parse(fs.readFileSync('./db/notes.json', 'utf8'));
 
 //reading notes.json file 
@@ -20,12 +19,21 @@ router.post('/notes', (req, res) => {
   let noteId = (savedNotes.length, uuid());
   newNote.id = noteId;
   savedNotes.push(newNote);
+  console.log('Note Saved!');
 
   fs.writeFileSync('./db/notes.json', JSON.stringify(savedNotes));
-  console.log(newNote)
   res.json(savedNotes);
 })
 
+//delete api
+router.delete('/notes/:id', (req, res) => {
+  let getNote = savedNotes.find(({ id }) => id === req.params.id);
+  savedNotes.splice(savedNotes.indexOf(getNote), 1);
 
+  fs.writeFileSync('./db/notes.json', JSON.stringify(savedNotes));
+  res.json(savedNotes);
+  console.log('Note Deleted!');
+
+})
 
 module.exports = router;
