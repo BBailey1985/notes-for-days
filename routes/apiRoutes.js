3f6dc95d018ffe3881e711f0646220806261708a
BBailey1985 <brandon_bailey@hotmail.com>
2022-01-24 11:52:47 -0600

    got POST working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,31 @@
 const router = require('express').Router();
+const uuid = require('../helpers/uuid')
 const fs = require('fs');
 const path = require('path');
 const savedNotes = JSON.parse(fs.readFileSync('./db/notes.json', 'utf8'));
 
 //reading notes.json file 
-router.get('/api/notes', (req, res) => {
-  console.log('Hello!')
+router.get('/notes', (req, res) => {
   res.json(savedNotes)
+})
 
-//   // res.getFile(path.join(__dirname, notes));
-// })
+// 
+router.get('/notes/:id', (req, res) => {
+  res.json(savedNotes[req.params.id]);
+})
 
-router.get("/api/notes", function (req, res) {
-  fs.readFile("./db/notes.json", "utf8", function (err, data) {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.json(notes);
-  });
-});
+// post api
+router.post('/notes', (req, res) => {
+  let newNote = req.body;
+  let noteId = (savedNotes.length, uuid());
+  newNote.id = noteId;
+  savedNotes.push(newNote);
 
-// router.get('/api/notes/:id', (req, res) => {
-//   let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
-//   res.json(storedNote[Number(req.params.id)]);
-// })
+  fs.writeFileSync('./db/notes.json', JSON.stringify(savedNotes));
+  console.log(newNote)
+  res.json(savedNotes);
+})
 
-// // post api
-// router.post('/api/notes', (req, res) => {
-//   let storedNote = JSON.parse(fs.readFileSync(notes, 'utf8')); 
-//   let newNote = req.body;
-//   let noteId = (storedNote.length).toString();
-//   newNote = noteId;
-//   storedNote.push(newNote);
 
-//   fs.writeFileSync(notes, JSON.stringify(storedNote));
-//   console.log(newNote)
-//   res.json(storedNote);
-// })
 
 module.exports = router;
